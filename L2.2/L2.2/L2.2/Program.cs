576442e6d56c316a2245f27cab59f2e4b9ed52bd
Nastya <nastyamtv32@gmail.com>
2022-05-20 12:00:53 +0300

    Task 2.2

--- L2.2/L2.2/L2.2/Program.cs
+++ L2.2/L2.2/L2.2/Program.cs
@@ -2,11 +2,64 @@
 
 namespace L2._2
 {
+    class Matrix 
+    {
+        public int n;
+        public int m;
+
+        public int[,] VerticalSnake() 
+        {
+            int[,] arr = new int[n,m];
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < m; j++) 
+                { 
+                    arr[i,j]=0;
+                }
+            }
+            int k = 1;
+            for (int j = 0; j < m; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i, j] = k;
+                        k++;
+                    }
+                }
+                if (j % 2 == 1) 
+                {
+                    for (int i = n-1; i >=0; i--)
+                    {
+                        arr[i, j] = k;
+                        k++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(arr[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            return arr;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Matrix Array = new Matrix();
+            Array.n = 3;
+            Array.m = 4;
+            Array.VerticalSnake();
         }
     }
 }
